d962e90c3d4e921d530851bb595fdaad1d6c6782
Leida_Gandy <leida.gandy@gmail.com>
2020-09-07 20:49:39 +0100

    Create authedUser action, authedUser reducer, and combineReducers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+ 
   return (
     <div className="App">
      
@@ -11,5 +13,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
---
+++ src/reducers/authedUser.js
@@ -0,0 +1,9 @@
+import { SET_AUTHED_USER } from "../actions/authedUser";
+
+export default function authedUser(state = null, action) {
+  if (action.type === SET_AUTHED_USER) {
+    return action.id
+  } else {
+    return state;
+  }
+}
