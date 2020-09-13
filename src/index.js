799cd5bbc4b48c2ea8deac00d6e520c61874e454
Leida_Gandy <leida.gandy@gmail.com>
2020-09-13 21:25:23 +0100

    Create addPoll, receivePoll and handlePool action creators

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import reducer from "./reducers"
+import reducer from "./reducers";
 
-
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-
