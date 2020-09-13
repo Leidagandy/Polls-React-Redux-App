bab42a23c3910d48df20b06e3245a3da61ae117f
Leida_Gandy <leida.gandy@gmail.com>
2020-09-13 23:02:07 +0100

    Add addpoll action creator

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -8,7 +8,20 @@ export default function users(state = {}, action) {
         ...state,
         ...action.users,
       };
+    case ADD_POLL:
+      const poll = action.poll;
+      const { author, id } = poll;
 
+      return {
+        ...state,
+        [author]: {
+          ...state[author], // keep the author the same
+          polls: state[author].polls.concat([id]),
+          // the state is going to be whatever is was but
+          // at the author polls property we want to add the id of the new poll
+          // onto the polls array
+        },
+      };
     default:
       return state;
   }
