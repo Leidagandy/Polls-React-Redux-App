aa11d8f02ae75306b83a0a2dde5a42c455c10aea
Leida_Gandy <leida.gandy@gmail.com>
2020-09-15 18:36:30 +0100

    Create leaderboard component

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,24 @@
-import * as React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { handleInitialData } from '../actions/shared'
+import * as React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import Leaderboard from "./Leaderboard";
 
 export default function App() {
-  const dispatch = useDispatch()
-  const loading = useSelector((state) => state.authedUser === null)
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.authedUser === null);
 
   React.useEffect(() => {
-    dispatch(handleInitialData())
-  }, [dispatch])
+    dispatch(handleInitialData());
+  }, [dispatch]);
 
-  const store = useSelector((store) => store)
-  console.log('Store', store)
+  //   const store = useSelector((store) => store)
+  // console.log('Store', store)
 
   return (
     <div>
-      {loading === true
-        ? null
-        : <div>Learn React</div>
-      }
+      {loading === true ? null : (
+          <Leaderboard />
+      )}
     </div>
-  )
-} 
+  );
+}
