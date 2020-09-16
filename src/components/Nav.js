bb900cf89793324027f98150058ae9cb4753c967
Leida_Gandy <leida.gandy@gmail.com>
2020-09-16 14:36:44 +0100

    Style nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,19 @@
-import * as React from "react"
-import { NavLink } from "react-router-dom"
+import * as React from "react";
+import { NavLink } from "react-router-dom";
+import "./Nav.css";
 
-
-export default function Nav () {
-    return (
-        <nav>
-            <NavLink to="/" exact>
-                Home
-            </NavLink>
-            <NavLink to="/leaderboard">
-            Leaderboard
-            </NavLink>
-            <NavLink to="/add">
-            Add Poll
-            </NavLink>
-        </nav>
-    )
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <NavLink to="/" exact activeClassName="active">
+        Home
+      </NavLink>
+      <NavLink to="/leaderboard" activeClassName="active">
+        Leaderboard
+      </NavLink>
+      <NavLink to="/add" activeClassName="active">
+        Add Poll
+      </NavLink>
+    </nav>
+  );
 }
